c71289313f352a235085317a44534c3584abe702
Jhordy Alexi <jhordydelaguila@hotmail.com>
2020-02-05 11:50:47 -0500

    BiFunction: BiFunction<?,?,?> NAME ; NAME.applY(args, args)

--- src/main/java/functionalinterface/_Function.java
+++ src/main/java/functionalinterface/_Function.java
@@ -1,10 +1,13 @@
 package functionalinterface;
 
+import java.util.function.BiFunction;
 import java.util.function.Function;
 
 public class _Function {
 
     public static void main(String[] args) {
+
+        // Function takes 1 argument and produces 1 result
         int increment = incrementByOne(1);
         System.out.println(increment);
 
@@ -16,6 +19,15 @@ public class _Function {
 
         Function<Integer, Integer> addBy1AndThenMultiplyBy10 = incrementByOneFunction.andThen(multiplyBy10Function);
         System.out.println(addBy1AndThenMultiplyBy10.apply(4));
+
+        // Function takes 2 arguments and produces 1 result
+        System.out.println(
+                incrementByOneAndMultiply(4, 100)
+        );
+
+        System.out.println(
+                incrementByOneAndMultiplyBiFunction.apply(4, 100)
+        );
     }
 
     static Function<Integer, Integer> multiplyBy10Function = number -> number * 10;
@@ -24,6 +36,13 @@ public class _Function {
 
     static int incrementByOne(int number) {
         return number + 1;
+    }
+
+    static BiFunction<Integer, Integer, Integer> incrementByOneAndMultiplyBiFunction =
+            (numberToIncrementByOne, numberToMultiplyBy) -> (numberToIncrementByOne + 1) * numberToMultiplyBy;
+
+    static int incrementByOneAndMultiply(int number, int numToMultiplyBy) {
+        return (number + 1) * numToMultiplyBy;
     }
 
 }
