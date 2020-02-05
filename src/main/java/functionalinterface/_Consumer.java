858354fffc469afda39e29f575e709660e75d212
Jhordy Alexi <jhordydelaguila@hotmail.com>
2020-02-05 12:29:46 -0500

    Consumer: Consumer<?> NAME; NAME.accept(?)

---
+++ src/main/java/functionalinterface/_Consumer.java
@@ -0,0 +1,35 @@
+package functionalinterface;
+
+import java.util.function.Consumer;
+
+public class _Consumer {
+
+    public static void main(String[] args) {
+        Customer maria = new Customer("Maria", "99999");
+        greetCustomer(maria);
+
+        greetCustomerConsumer.accept(maria);
+    }
+
+    static Consumer<Customer> greetCustomerConsumer = customer ->
+            System.out.println("Hello " + customer.customerName +
+                    ", thanks for registering phone number "
+                    + customer.customerPhoneNumber);
+
+    static void greetCustomer(Customer customer) {
+        System.out.println("Hello " + customer.customerName +
+                ", thanks for registering phone number "
+                + customer.customerPhoneNumber);
+    }
+
+    static class Customer {
+        private final String customerName;
+        private final String customerPhoneNumber;
+
+        Customer(String customerName, String customerPhoneNumber) {
+            this.customerName = customerName;
+            this.customerPhoneNumber = customerPhoneNumber;
+        }
+    }
+
+}
